566d01785d385a30d7a23007098848f190378f71
Abhilash Sharma <abhi.recoil2051@gmail.com>
2026-05-01 15:38:20 +0530

    some static changes to trader basket

--- trader-basket/app/sharepricecalculation/page.tsx
+++ trader-basket/app/sharepricecalculation/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function SharePriceCalculation() {
   return (
     <div>
       <h1>Share Price Calculation</h1>
       <p>This page will display the share price calculation results.</p>
+      <Link href="/" className="text-blue-600 hover:underline"> Back </Link>
     </div>
   );
 }
